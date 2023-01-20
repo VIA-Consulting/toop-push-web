4d271e11f1222297cad92f6fdea500ceaecfa2e7
Andre Lima <andre.lima@viaconsultingway.com>
2023-01-20 14:41:08 +0000

    feat: modal

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ paymentButtons.forEach((button, index) => {
     button.classList.add("active");
 
     if (index === 2) {
+      document.querySelector("#phone-container").classList.add("active");
       document.querySelector("#phone").classList.add("active");
     } else {
+      document.querySelector("#phone-container").classList.remove("active");
       document.querySelector("#phone").classList.remove("active");
     }
   });
@@ -19,10 +21,8 @@ paymentButtons.forEach((button, index) => {
 const comerciante = document.querySelector("#comerciante").innerHTML;
 const total = document.querySelector("#total").innerHTML;
 const phoneNumber = document.querySelector("#number");
-const submitButton = document.querySelector("#submit");
-submitButton.parentNode.style.display = "flex";
-submitButton.parentNode.style.justifyContent = "center";
-submitButton.addEventListener("click", () => {
+const enviarButton = document.querySelector("#enviar");
+enviarButton.addEventListener("click", () => {
   if (phoneNumber.value.length !== 0) {
     alert(
       `Comerciante: ${comerciante}\nTelemóvel: ${
@@ -31,6 +31,16 @@ submitButton.addEventListener("click", () => {
     );
     phoneNumber.value = "";
     paymentButtons[2].classList.remove("active");
+    document.querySelector("#phone-container").classList.remove("active");
     document.querySelector("#phone").classList.remove("active");
+  } else {
+    alert("Por favor insira um número de telemóvel");
   }
+});
+
+const phoneContainer = document.querySelector("#phone-container");
+phoneContainer.addEventListener("click", () => {
+  paymentButtons[2].classList.remove("active");
+  document.querySelector("#phone-container").classList.remove("active");
+  document.querySelector("#phone").classList.remove("active");
 });
